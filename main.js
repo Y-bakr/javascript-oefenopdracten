a21d628b982e11287a2692009e03bf2657bd3da6
Y-bakr <302786836@student.rocmondriaan.nl>
2023-09-14 14:18:39 +0200

    javascript oefenopdrachten
    ..

---
+++ main.js
@@ -0,0 +1,43 @@
+console.log('main.js is running')
+
+
+//oefenopdracht 1
+
+let m=120;
+let n=4
+
+let result= m*n
+
+console.log ('result is ', result)
+
+//oefenopdracht 2
+
+let brand= 'Audi'
+
+let model= 'Rs7'
+
+let carname= brand+''+model
+
+console.log('The name of the car is', carname)
+
+//opdracht 3
+
+let totalstudents=24
+totalstudents= 21;
+console.log('Totaal aantaal leerlingen is'), totalstudents
+
+
+//opdracht 4
+
+const x= 'laptop'
+
+if(x == 'laptop') {
+    console.log ('laptop')
+
+} else{console.log ('disktop')
+}
+
+//opdracht 5
+
+const h1Elemnt =document.querySelector ('.title')
+h1Elemnt.textContent='Economie'
