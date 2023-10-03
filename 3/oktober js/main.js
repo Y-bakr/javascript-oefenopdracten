730ae62ac21d5297521d635ea17c6d3470515c57
Y-bakr <302786836@student.rocmondriaan.nl>
2023-10-03 12:50:55 +0200

    3/oktober javascript

---
+++ 3/oktober js/main.js
@@ -0,0 +1,87 @@
+//opdracht 1
+
+let counter = 0;
+while ( counter <= 9) {
+console.log("counter", counter);
+// counter+= 1;
+
+counter++;
+
+}
+
+
+// opdracht 3
+
+counter = 10;
+while ( counter >= 0)
+
+{console.log ("counter", counter);
+
+counter--;
+
+}
+
+
+//opdracht 7 
+
+counter = 0;
+
+
+
+while(true){
+
+if (counter > 9 ){
+
+break;
+
+}
+
+console.log("counter", counter);
+counter++;
+ }
+ console.log ("einde loop opdracht7");
+
+
+ //opdracht 8
+
+counter = 0;
+
+while(counter >20) {
+console.log("counter", counter);
+
+if(counter <9 ){
+
+        break;
+
+}
+
+counter++;
+
+
+
+}
+console.log ("einde loop opdracht8");
+
+
+//opdracht 9
+
+counter = 0
+
+while ( counter < 20 ) {
+
+    counter++;
+
+    console.log("Dit is zin een ");
+
+    if ( counter > 10 ) {
+        continue;
+
+    }
+
+
+    console.log("Dit is zin twee ");
+
+}
+
+
+
---
+++ new les/main.js
@@ -0,0 +1,101 @@
+//test 1
+
+let counter = 0;
+
+while ( counter < 10 ) {
+
+console.log("loop: ", counter);
+counter++;
+
+}
+
+for(let i = 0; i <10; i++ ) {
+console.log("for-loop: ", i );
+
+
+}
+
+
+for ( let counter = 0; counter < 10; counter++){ 
+
+console.log("for-loop v2: ", counter);
+
+
+}
+
+const todos=  ["trinen", "huiswerk", "booschappen", "film kijken"]
+
+for(let m = 0; m< todos.length; m++) {
+
+const task = todos[ m ];
+
+console.log("---"+task+"---");
+
+}
+
+
+
+//opdracht 1 
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
+  
+  console.log("Einde programma");
+
+  //opdracht 2
+
+  for (let i = 0; i <= 20; i++) {
+    console.log(i);
+  }
+  
+  console.log("Einde programma");
+
+  //opdracht 3
+
+  for (let i = 2; i <= 20; i++) {
+    console.log(i);
+  }
+  
+  console.log("Einde programma");
+
+  //opdrahcht 4
+
+  for (let i = 10; i >= 0; i--) {
+    console.log(i);
+  }
+  
+  console.log("Einde programma");
+
+  //opdrahct 5
+
+  for (let i = 40; i >= 4; i--) {
+    console.log(i);
+  }
+  
+  console.log("Einde programma");
+
+  //opdracht 6
+
+  for (let i = 0; i < 8; i++) {
+    console.log("Ik ontwikkel apps");
+  }
+
+  //opdracht 7
+
+  for (let i = 0; i <= 12; i++) {
+    console.log("Dit is loop: " + i);
+  }
+
+  //opdracht 8
+
+  for (;;) {
+    for (let i = 0; i < 10; i++) {
+    console.log(i);
+    }
+    break;
+    }
+    
+    console.log("Einde programma");
+
+    
